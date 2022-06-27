40579c682eafd5dbd8e83347be6d1d61bbbf0a6d
Student9876 <shouvik9876@gmail.com>
2022-06-27 15:06:14 +0530

    Rename this

--- 01_hello.py
+++
@@ -1,5 +0,0 @@
-# Author: ZEON  #<-- This symbol is used for single line comment
-import os            #Importing Modules
-import flask
-print("Hello World")  #print function
-''' This triple single quote is used for multiline comment'''
