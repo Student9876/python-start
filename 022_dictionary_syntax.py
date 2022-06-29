1214216bc69b6c3e4c88a497af79774f1ccf888c
Student9876 <shouvik9876@gmail.com>
2022-06-29 15:33:17 +0530

    Dictionary Starts

---
+++ 022_dictionary_syntax.py
@@ -0,0 +1,15 @@
+# Dictinary is collection of Key- Value pairs
+
+from lib2to3.refactor import MultiprocessingUnsupported
+
+
+myDict = {
+    "ZEON": "Currently a programming Learner",
+    "Fast": "In a quick manner",
+    "Marks": [1,2,5],
+    "anotherDict": {"zeon":"gamer"}
+}
+print(myDict['ZEON'])
+print(myDict['Fast'])
+print(myDict['Marks'])
+print(myDict["anotherDict"])
