bfa0061ecd877f315b6dfa84c45b24fd761dd7e3
Student9876 <shouvik9876@gmail.com>
2022-07-03 22:31:37 +0530

    New chapters

---
+++ 022_conditionals_if-elif-else.py
@@ -0,0 +1,28 @@
+a = 8
+
+# 1. If-elif-else ladder 
+# if(a<3):
+#     print("The value of a is greater than 3")
+# elif(a>7):
+#     print("The value of a is greater than 7")
+# elif(a>13):
+#     print("The value of a is greater than 13")
+# elif(a>17):
+#     print("The value of a is greater than 17")
+# else:
+#     print("The value of a is not greater then 3")
+
+
+# 2. Multiple if statements
+if(a<3):
+    print("The value of a is greater than 3")
+if(a>7):
+    print("The value of a is greater than 7")
+if(a>13):
+    print("The value of a is greater than 13")
+if(a>17):
+    print("The value of a is greater than 17")
+else:
+    print("The value of a is not greater then 3 or 7")
+
+
---
+++ 025_is_else_optional.py
@@ -0,0 +1,6 @@
+a = 6
+if(a==7):
+    print("Yes")
+elif(a>45):
+    print("no and yes")
+#it will not show any error if condition doesn't meet
