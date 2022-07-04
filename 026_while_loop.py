3561b7a80ef06deda56e1c5147e3ecf750b7cb2d
Student9876 <shouvik9876@gmail.com>
2022-07-04 10:46:00 +0530

    Loops

---
+++ 026_while_loop.py
@@ -0,0 +1,6 @@
+# This loop will continue untill the condition becomes False
+i = 0
+while i<10:
+    print("Yes " + str(i))
+    i = i + 1
+print("Done") #Ignored the indentation to make the print("Done") separate from the loop
---
+++ 029_Range_forLoop.py
@@ -0,0 +1,10 @@
+# Range function of for loop 
+
+for i in range(8): # by default it starts from 0
+    print(i)
+
+for i in range(1, 8): #loop will start from 1
+    print(i)
+
+for i in range(1, 8, 2): #Here we included step size at last. It will print every 2nd number
+    print(i)
