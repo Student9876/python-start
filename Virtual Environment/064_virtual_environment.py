5757662d0b8dbb59ee7d7c9a1cd4b897b5c1ca83
Student9876 <shouvik9876@gmail.com>
2022-07-12 14:10:13 +0530

    Virtual Environment

---
+++ Virtual Environment/064_virtual_environment.py
@@ -0,0 +1,12 @@
+'''
+For linux user: source venv/bin/activate
+For Windows powershell user: ./venv/Scripts/Activate.ps1
+
+if there is a problem eith Execution policy settings.
+Fix : Set-ExecutionPolicy Unrestricted -Force
+in powershell As administrator
+
+'''
+import flask #flask - 0.5.2
+import pandas as pd
+import pygame 
